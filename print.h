a7581f9557f2a5b6c694a3af9c99424971d28cbc
vic-ky8 <117303953+vic-ky8@users.noreply.github.com>
2023-06-27 15:52:59 +0100

    Update and rename main.h to print.h

---
+++ print.h
@@ -0,0 +1,20 @@
+#ifndef PRINT_H
+#define PRINT_H
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+/**
+  * struct print - typedef for printing various type
+  * @opt: type of print
+  * @call_fun: function to print
+  */
+typedef struct format
+{
+	char *opt;
+	int (*call_fun)(va_list);
+} convert;
+
+
+int _printf(const char *format, ...);
+
+#endif 
